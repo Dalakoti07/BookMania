f300060f31a06bbd5d3b9ceadd042457d31717bf
LoneWolf <saurabhdalakoti07@gmail.com>
2019-03-03 01:53:57 +0530

    GUI improved ,api link and book details added, one last nail to the coffin is that -add review system and login sign on the left to nav bar, that depends upon databases and sessions

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ Prevent the users from making account with invalid username and password and use
 handle various cases
 '''
 
-from flask import Flask, session,render_template,request
+from flask import Flask, session,redirect,url_for,render_template,request,jsonify
 import requests
 from flask_session import Session
 from sqlalchemy import create_engine
@@ -18,6 +18,19 @@ app.config["SESSION_TYPE"]="filesystem"
 app.config['SECRET_KEY']='super secret key'
 
 username=""
+
+def helper(isbn):
+	isbn='%'+isbn+'%'
+	query=db.execute("SELECT * FROM book_record where id like :isbn" , {"isbn": isbn}).fetchall()
+	db.commit()
+	book=query[0]
+	book=list(book)
+	res=requests.get("https://www.goodreads.com/book/review_counts.json",params={"key":"44ZCyFojgwqbYPzQ2vfw","isbns":book[0]})
+	rating=res.json()['books'][0]['average_rating']
+	work_reviews_count=res.json()['books'][0]['reviews_count']
+	book.append(rating)
+	book.append(work_reviews_count)
+	return book
 
 # set the database
 engine = create_engine("postgres://wxwjtpitggoati:8dfb1487bafbda798870df37a4691bf46afd7162e0d8f3932e7ae20854cd4f89@ec2-54-75-230-41.eu-west-1.compute.amazonaws.com:5432/d50618rt42i32c")
@@ -41,14 +54,14 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/")
 def index():
 	if username!="" :
-	    return render_template('home2.html',name=session[username])
+	    return render_template('home.html',name=session[username])
 	else:
-	    return render_template('home2.html',name="")
+	    return render_template('home.html',name="")
 
 # this is the login page
 @app.route("/login")
 def login():
-	return render_template('login2.html')
+	return render_template('login.html')
 
 # this woul be the first page seen by the user when he is loged in
 @app.route("/welcome",methods=["POST","GET"])
@@ -60,19 +73,25 @@ def welcome():
 	password_from_db=db.execute("SELECT password FROM users_record where username =:username", {"username": username}).fetchone()
 	# if we donot find any user then prompt the page saying that wrong credentials entered ,and give the option of forget password
 	if password_from_db is None:# means there is no user
-		return render_template('register.html')
+		return render_template('register2.html')
 	if password_from_db[0]!=password:
-		return render_template('login2.html',message="U have forgot your password or username")
+		return redirect(url_for('login'))
+		#return render_template('login2.html',message="U have forgot your password or username")
 	# make entry in the session
 	session['username']=username
-	return render_template('welcome2.html',name=name)
+	return redirect(url_for('success'))
+	#return render_template('welcome2.html')
+
+@app.route("/success")
+def success():
+	return render_template('success.html')
 
 @app.route("/logout")
 def logout():
 	# make the user log out from the 
 	# remove the username from the session if it is there
     session.pop('username', None)
-    return render_template('home.html')
+    return redirect('home.html')
 
 # this is the registration page
 @app.route("/register")
@@ -80,7 +99,7 @@ def register():
 	# if u are creating a new account then logout from current session
 	logout()
 	# what is happening is home page is being and then register.html is being rendered
-	return render_template('register2.html',register="")
+	return render_template('register.html',register="")
 
 @app.route("/create_account",methods=["POST","GET"])
 def create_account():
@@ -97,22 +116,22 @@ def create_account():
 		db.commit()
 		# creating a session here , that is when user has made a new account
 		session['username']=username
-		return render_template('welcome2.html',name=name+" Your account has been created")
+		return render_template('welcome.html',name=name+" Your account has been created")
 	else:
 		return render_template('register.html',message="Please try with different user name")
 
 @app.route("/search")
 def search():
-	return render_template('search_query2.html')
+	return render_template('search.html')
 
-@app.route("/fetch_data",methods=["POST","GET"])
-def fetch_data():
+@app.route("/fetchdata",methods=["POST","GET"])
+def fetchdata():
 	# what if user didnot apply any filter
 	x=request.form.get("ISBN")
 	y=request.form.get("author")
 	z=request.form.get("title")
 	isbn=author=title=""
-	if len(x)>0 and len(y)>0 and len(z)>0:
+	if x!=None and y!=None and z!=None:
 		# if user has used all the valid filters
 		isbn=x
 		isbn='%'+isbn+'%'
@@ -122,14 +141,14 @@ def fetch_data():
 		title='%'+title+'%'
 		booklist=db.execute("SELECT * FROM book_record where id like :isbn and title like :title and author like :author" , {"isbn": isbn,"title":title,"author":author})
 
-	elif len(y)==0 and len(x)>=1 and len(z)>=1:
+	elif y==None and x!=None and z!=None:
 		# if isbn and title are valid
 		isbn=x
 		isbn='%'+isbn+'%'
 		title=z
 		title='%'+title+'%'
 		booklist=db.execute("SELECT * FROM book_record where id like :isbn and title like :title " , {"isbn": isbn,"title":title})
-	elif len(z)==0 and len(x)>=1 and len(y)>=1:
+	elif z==None and x!=None and y!=None:
 		# if we have valid isbn and author only
 		isbn=x
 		isbn='%'+isbn+'%'
@@ -137,13 +156,25 @@ def fetch_data():
 		author='%'+author+'%'
 		booklist=db.execute("SELECT * FROM book_record where id like :isbn and author like :author" , {"isbn": isbn,"author":author})
 	
-	elif len(x)==0 and len(y)>0 and len(z)>0:
+	elif x==None and y!=None and z!=None:
 		# we have valid author and title only
 		author=y
 		author='%'+author+'%'
 		title=z
 		title='%'+title+'%'
 		booklist=db.execute("SELECT * FROM book_record where title like :title and author like :author" , {"title":title,"author":author})
+	elif x==None and y==None and z!=None:
+		title=z
+		title='%'+title+'%'
+		booklist=db.execute("SELECT * FROM book_record where title like :title" , {"title":title})
+	elif y==None and z==None and x!=None:
+		isbn=x
+		isbn='%'+isbn+'%'
+		booklist=db.execute("SELECT * FROM book_record where isbn like :isbn" , {"title":isbn})
+	elif x==None and z==None and y!=None:
+		author=y
+		author='%'+author+'%'
+		booklist=db.execute("SELECT * FROM book_record where author like :author" , {"author":author})
 	else:
 		return '<h1>Enter some valid filters, you didnot apply any of them </h1>'
 	
@@ -154,22 +185,37 @@ def fetch_data():
 	list_book=[]
 	for b in booklist:
 		list_book.append(b)
+	#return redirect(url_for('.do_foo', messages=messages))
 	return render_template('booklist.html',booklist=list_book)
 	#return str(list_book)
 	# too much work for api donot do it hear booklist=get_info(list_book)
 	#print(booklist)
 
-'''@app.route('/book/<string:isbn>')
-def book_data():
-'''
+@app.route('/book/<string:isbn>')
+def book_data(isbn):
+	book=helper(isbn)
+	return render_template('details.html',details=book)
+
+@app.route('/api/<string:isbn>')
+def apis(isbn):
+	book=helper(isbn)
+	# id title autjor year
+	return jsonify({
+		"title":book[1],
+		"author":book[2],
+		"year":book[3],
+		"isbn":book[0],
+		"review_count":book[5],
+		"average_score":book[4]
+	})
 
 
 # We donot require it @app.route("/api_request",methods=["POST"])# only the pages can ask for the details
-def get_info(booklist):
-	'''
+'''def get_info(booklist):
+	
 	data_dict=res.json()
 	print(data_dict['books'][0])# this gives u access to the dictionary now give third parameter as isbn or id or rating count or etc
-	'''
+	
 	# get the average score for each book
 	# what we should do is make a page for search about the books and then get the information from database into a list and that list passed to this function and then we append more data to it about ,average rating and then at last rendering the page and giving it the data to display in beautiful table format
 	final_list=[]
@@ -185,3 +231,4 @@ def get_info(booklist):
 		else :# in case we have no ratng for a book
 			final_list.append(b)
 	return final_list
+'''
